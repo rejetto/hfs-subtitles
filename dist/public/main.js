5a44a6b5433f51e3d841d2f7b752173d19acb255
Massimo Melina <a@rejetto.com>
2025-08-29 16:24:15 +0200

    button for custom link

--- dist/public/main.js
+++ dist/public/main.js
@@ -1,23 +1,38 @@
 "use strict";{
     const SUPPORTED_EXTS = ['srt', 'vtt']
+    const {h, React} = HFS
     HFS.onEvent('fileShow', params => {
         const { entry, Component } = params
         const folder = entry.uri.slice(0, -entry.name.length)
         const noExt = getNoExt(entry)
         const subEntries = HFS.state.list?.filter(x =>
             SUPPORTED_EXTS.includes(x.ext) && x.uri.startsWith(folder) && getNoExt(x).startsWith(noExt) )
-        if (!subEntries?.length) return
-        params.Component = props =>
-            HFS.h(Component, props,
-                subEntries.map(e => {
-                    const title = getNoExt(e).slice(noExt.length).replace(/^[-. ]+/, '') || "unknown"
-                    return HFS.h('track', {
-                        label: title,
-                        kind: 'subtitles',
-                        srcLang: title,
-                        src: e.uri + (e.ext === 'vtt' ? '' : '?get=vtt'), // external file
-                    })
-                }) )
+        const customLabel = HFS.t("Enter link to subtitles")
+        params.Component = props => {
+            const [custom, setCustom]  = React.useState()
+            return h(React.Fragment, {},
+                h(Component, props,
+                    custom && h('track', {
+                        kind: 'subtitles', label: "custom", srcLang: "custom", default: true,
+                        src: custom + (!custom.endsWith('srt') ? '' : '?get=vtt'),
+                    }),
+                    subEntries?.map(e => {
+                        const title = getNoExt(e).slice(noExt.length).replace(/^[-. ]+/, '') || "unknown"
+                        return h('track', { key: title,
+                            kind: 'subtitles', srcLang: title, label: title,
+                            src: e.uri + (e.ext === 'vtt' ? '' : '?get=vtt'),
+                        })
+                    }) ),
+                subEntries?.length > 0 && HFS.t("Subtitles found: {n}", { n: subEntries?.length }),
+                h('button', {
+                    style: { fontSize: 'small' },
+                    onClick() {
+                        HFS.dialogLib.promptDialog(customLabel, { value: custom }).then(x =>
+                            x !== undefined && setCustom(x))
+                    }
+                }, customLabel)
+            )
+        }
 
         function getNoExt(entry) {
             return entry.name.slice(0, -entry.ext.length-1).toLowerCase()
