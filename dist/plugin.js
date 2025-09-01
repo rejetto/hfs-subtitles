939fbf2710b529338b3a71785e0dd9c328a74734
Massimo Melina <a@rejetto.com>
2025-09-01 22:10:30 +0200

    Support embedded subtitles

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
-exports.version = 1
+exports.version = 1.1
 exports.description = "load subtitles showing videos. The subtitles file is automatically loaded if it has the same name of the video file."
 exports.apiRequired = 12.9
 exports.repo = "rejetto/hfs-subtitles"
@@ -6,18 +6,46 @@ exports.frontend_js = "main.js"
 exports.frontend_css = "style.css"
 exports.afterPlugin = 'unsupported-videos' // or we'll skip those extensions not supported directly by hfs
 exports.changelog = [
+    { "version": 1.1, "message": "Support embedded subtitles" },
     { "version": 1.0, "message": "Support non-utf8 subtitles" },
     { "version": 0.9, "message": "Search subtitles in subfolders" }
 ]
+exports.config = {
+    ffmpeg_path: { type: 'real_path', fileMask: 'ffmpeg*', label: "FFmpeg path", helperText: "Specify where FFmpeg is installed to support embedded subtitles" }
+}
 
 exports.init = api => {
     const { convert } = api.require(__dirname +'/subtitle-converter.bundle.js')
     const { detect } = api.require(__dirname +'/jschardet.min.js')
     const { decode } = api.require('iconv-lite')
     const consumers = api.require('stream/consumers')
+    const fs = api.require('fs/promises')
+    let { execFile } = api.require('child_process')
+    execFile = api.require('util').promisify(execFile)
     return {
         middleware: ctx => async () => {
             if (!ctx.body) return // in case of 304
+            if (ctx.query.get === 'subtitles') {
+                const ff = api.getConfig('ffmpeg_path')
+                if (!ff) return
+                const path = ctx.state.fileSource
+                const {idx} = ctx.query
+                if (idx !== undefined) {
+                    const fn = `hfs_sub_${Math.random()}.vtt`
+                    await execFile(ff, ['-i', path, '-y', '-map', `0:s:${idx}`, fn])
+                    ctx.body = await fs.readFile(fn)
+                    fs.unlink(fn)
+                }
+                else {
+                    const out = await execFile(ff, ['-i', path]).then(x => x.stdout, e => e.stderr)
+                    const arr = out.split(/^\s+Stream #/m)
+                    ctx.body = arr.map(s => {
+                        const m = /^\d+:\d+(?:\((\w+)\)).+Subtitle?/.exec(s)
+                        return m && { lang: m[1], title: /\btitle\s*:\s*(.+)/.exec(s)?.[1] }
+                    }).filter(Boolean)
+                }
+                return ctx.stop()
+            }
             if (ctx.query.get !== 'vtt') return
             const buffer = await consumers.buffer(ctx.body)
             const {encoding} = detect(buffer)
--- dist/public/main.js
+++ dist/public/main.js
@@ -2,6 +2,7 @@
     const SUPPORTED_EXTS = ['srt', 'vtt', 'dfxp', 'scc', 'ttml', 'ssa', 'ass']
     const search = SUPPORTED_EXTS.map(x => `*.${x}`).join('|')
     const {h, React, _} = HFS
+    const cache = {}
     HFS.onEvent('fileShow', params => {
         const { entry, Component } = params
         if (Component !== HFS.fileShowComponents.Video && !Component?.hfs_show_video) return
@@ -12,9 +13,6 @@
         const noExt = getNoExt(entry)
         const subEntries = HFS.state.list?.filter(x =>
             SUPPORTED_EXTS.includes(x.ext) && x.uri.startsWith(folderUri) && getNoExt(x).startsWith(noExt) )
-        // search for more subs, both useful for subfolders and in case the show was started from a partial list
-        const req = HFS.apiCall('get_file_list', { uri: folderUri, search }).then(res =>
-            res.list.map(x => mapEntry(new HFS.DirEntry(x.n, x))))
 
         function mapEntry(e) {
             let label = getNoExt(e)
@@ -31,7 +29,21 @@
         params.Component = React.forwardRef((props, ref) => {
             const [subs, setSubs] = React.useState(() => subEntries?.map(mapEntry) || [])
             React.useEffect(() => { // copy new subs to the state, only if not already present
-                req.then(res => setSubs(was => [...was, ...res.filter(x => !_.some(was, { src: x.src }))]))
+                const reqs = cache[entry.uri] ||= [
+                    // search for more subs, both useful for subfolders and in case the show was started from a partial list
+                    HFS.apiCall('get_file_list', { uri: folderUri, search }).then(res =>
+                        res.list.map(x => mapEntry(new HFS.DirEntry(x.n, x)))),
+                    fetch(entry.uri + '?get=subtitles')
+                        .then(res => res.json())
+                        .then(arr => arr.map((x,i) => ({
+                            srcLang: x.lang,
+                            label: x.title,
+                            src: entry.uri + '?get=subtitles&idx=' + i,
+                        })))
+                ]
+                for (const req of reqs)
+                    req.then(res => setSubs(was =>
+                        was.concat(res.filter(x => !_.some(was, { src: x.src }))) ))
             }, [])
             const ref2 = React.useRef()
             return h(React.Fragment, {},
